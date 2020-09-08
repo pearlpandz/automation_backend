bd52db927cfc828e4aaf110c6d93c6d45319d789
Muthupandi <muthukrishnan.j@servion.com>
2020-09-08 15:44:33 +0530

    access control allow origin update

--- server.js
+++ server.js
@@ -21,18 +21,26 @@ mongoose.connect(CONFIG.DB_URL, { useNewUrlParser: true }).then(() => {
 
 // NPM Packages use configuration
 app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(bodyParser.json());
 
-app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb'}));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb' }));
 
 app.use(bodyParser.json({ limit: '100mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true, parameterLimit:500000 }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true, parameterLimit: 500000 }));
 
 // given full permission to access this folder
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     // console.log('fullUrl = ', fullUrl)
     next();
