e3ef1bf7be59c6cc08e817be0f9b441f98003ea1
Muthupandi <muthukrishnan.j@servion.com>
2020-09-08 16:26:29 +0530

    cors

--- server.js
+++ server.js
@@ -20,12 +20,9 @@ mongoose.connect(CONFIG.DB_URL, { useNewUrlParser: true }).then(() => {
 
 
 // NPM Packages use configuration
-app.use(cors());
+// app.use(cors());
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
