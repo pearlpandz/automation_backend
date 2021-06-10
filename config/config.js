c539a2406233bf8feaaae8e9c974f5f17e0efa7e
Muthupandi V <pearlpandzz@gmail.com>
2021-06-10 12:32:12 +0530

    database update

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     host: "localhost", // "iot.c3yfy0oqxffp.ap-south-1.rds.amazonaws.com", 
     port: 3306,
     user: 'root', // "root", 
-    password: "", // "Muthu@pand1", 
+    password: "Muthu@pand1", 
     node_port: 8000,
     database: 'iot',
     secret: "youcanfindme",
